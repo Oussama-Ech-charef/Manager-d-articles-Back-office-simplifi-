4c830311ffe4da4829304e5ad042e3e0d2eda1cc
OUSSAMA <oussamacharef71@gmail.com>
2026-03-06 12:43:15 +0100

    add modification to chapitere 2

--- les_chapitere/Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/liste_utilisateurs.php
+++ les_chapitere/Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/liste_utilisateurs.php
@@ -4,7 +4,7 @@ require 'connexion.php';
 
 
 try {
-    $sql = "SELECT * FROM Utilisateur";
+    $sql = "SELECT * FROM users";
 
     $stmt = $pdo->query($sql);
 
@@ -19,16 +19,18 @@ try {
 echo "<table border='1'>
 
 <tr>
-<th>ID</th>
+<th>ID_USER</th>
 <th>Nom</th>
 <th>Email</th>
+<th>Date_birth</th>
 </tr>";
 
 foreach ($utilisateurs as $user) {
     echo "<tr>
-    <td>{$user['id']}</td>
-    <td>{$user['nom']}</td>
+    <td>{$user['id_user']}</td>
+    <td>{$user['user_name']}</td>
     <td>{$user['email']}</td>
+    <td>{$user['date_birth']}</td>
     </tr>";
 }
 
