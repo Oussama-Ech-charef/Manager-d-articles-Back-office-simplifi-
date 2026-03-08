173e43290eb541da8dd68b15b8a66fb57ff4aa22
OUSSAMA <oussamacharef71@gmail.com>
2026-03-08 23:07:10 +0000

    add modification to chapitere 3

--- les_chapitere/Chapitre 3 – Insérer, mettre à jour et supprimer des enregistrements/actions_utilisateurs.php
+++ les_chapitere/Chapitre 3 – Insérer, mettre à jour et supprimer des enregistrements/actions_utilisateurs.php
@@ -4,6 +4,9 @@
 
     require 'connexion.php';
 
+
+    echo "<br>";
+
 try {
 
     $stmt = $pdo->prepare("INSERT into users (user_name, email) values (:user_name, :email)");
@@ -12,8 +15,57 @@ try {
         'email' => 'oussama@email.com'
     ]);
     echo "user ajouté avec succés.";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+    $stmt = $pdo->prepare("UPDATE users set user_name = :user_name, email = :email where id_user = :id_user");
+
+    $stmt->execute([
+        'user_name' => 'Hassanee',
+        'email' => 'hassanee@gmail.com',
+        'id_user' => 2
+    ]);
+    echo "user mis à jour";
+
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+    $stmt = $pdo->prepare("DELETE from users where id_user = :id_user");
+
+    $stmt->execute(['id_user' => 12]);
+
+    echo "user supprimé.";
+
+
+    echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+echo $stmt->rowCount() . " ligne(s) affectée(s).";
+
+
+
+
 }catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
+
+
+
+
+
+
+
+
 
 ?>
